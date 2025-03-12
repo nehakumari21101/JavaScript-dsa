8346e8ac70eca017705e978cd706720d42b54d72
Neha kumari <kumarineha122162@gmail.com>
2025-03-12 21:33:17 +0530

    fibonacci number

--- 2-fabonacci.js
+++ 2-fabonacci.js
@@ -1,10 +1,17 @@
-var fib = function (n) {
-  const arr = [0, 1];
+// var fib = function (n) {
+//   const arr = [0, 1];
 
-  for (let i = 2; i <= n; i++) {
-    arr.push(arr[i-1] + arr[i - 2]);
-  }
-  return arr[n];
-//   console.log(arr);
-};
-fib(5);
+//   for (let i = 2; i <= n; i++) {
+//     arr.push(arr[i-1] + arr[i - 2]);
+//   }
+//   return arr[n];
+// };
+// fib(5);
+
+// using recursion
+const fib = function(n){
+    if(n<=1) return n;
+
+    return fib(n-1) + fib(n-2);
+}
+console.log(fib(3));
