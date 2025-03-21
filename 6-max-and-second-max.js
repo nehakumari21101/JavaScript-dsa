314fe16fcdd6e78828e1ee0e6d00c6ef9446cdb9
Neha kumari <kumarineha122162@gmail.com>
2025-03-21 21:17:16 +0530

    Second largest Number

--- 6-max-and-second-max.js
+++ 6-max-and-second-max.js
@@ -4,17 +4,43 @@
 // Input: [12, 35, 1, 10, 34, 1] --->> Output: 34
 // Input: [10, 5, 10]            --->> Output: 5
 
-function secondLargest(arr){
-    const uniqueArr = Array.from(new Set(arr));
+// function secondLargest(arr){
+//     const uniqueArr = Array.from(new Set(arr));
 
-    uniqueArr.sort((a,b) => {
-        return b-a;
-    })
+//     uniqueArr.sort((a,b) => {
+//         return b-a;
+//     })
 
-    if(uniqueArr.length >= 2){
-        return uniqueArr[1];
-    }else {
-        return -1;
+//     if(uniqueArr.length >= 2){
+//         return uniqueArr[1];
+//     }else {
+//         return -1;
+//     }
+// }
+
+// console.log(secondLargest([12, 35, 1, 10, 34, 1]))
+// Time Complexity -> O(nlogn);
+// Space complexity -> O()
+
+
+
+// Optimize Approach
+function secondLargestOptimised(arr){
+    let largest = Number.NEGATIVE_INFINITY;
+    let secondLargest = Number.NEGATIVE_INFINITY;
+
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] > largest){
+            secondLargest = largest;
+            largest = arr[i];
+        }else if(arr[i] != largest && arr[i] > secondLargest){
+            secondLargest = arr[i];
+        }
     }
+
+    return secondLargest;
 }
-console.log(secondLargest([12, 35, 1, 10, 34, 1]))
+
+console.log(secondLargestOptimised([10, 5, 10]));
+// Time complexity -> O(n);
+// Space complexity -> O(1);
